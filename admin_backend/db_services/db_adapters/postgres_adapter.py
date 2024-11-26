2b63e5c9a7eb079e9fed36e227f1a09a92f4b93e
Егор Марков <e.markov@gt-sot.ru>
2024-11-27 00:15:50 +0300

    FEATURE: add base API for DB operations, add validation

--- admin_backend/db_services/db_adapters/postgres_adapter.py
+++ admin_backend/db_services/db_adapters/postgres_adapter.py
@@ -1,6 +1,18 @@
+from typing import Any, Optional, re
+
+
 class PostgresqlRepository:
 
-    def __init__(self, dsn: str, query: str) -> None:
-        self._q = query
+    _qry_check = re.compile(".*(INSERT|UPDATE|DELETE|TRUNCATE).*")
+
+    @classmethod
+    def query_is_readonly(cls, q: str) -> bool:
+        if q == "" or cls._qry_check.match(q) is not None:
+            return False
+        return True
+
+    def __init__(self, dsn: str) -> None:
         self._dsn = dsn
+
+    async def load(self, q: str, *, records_limit: Optional[int] = None) -> list[tuple[Any]]: return ()
 
--- admin_backend/db_services/requests/requests.py
+++ admin_backend/db_services/requests/requests.py
@@ -1,6 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class SqlQuery(BaseModel):
     query: str
-    dsn: str
+    db_provider: str
+    database_name: str
+    db_host: str
+    db_port: str
+    db_user: str
+    db_user_passwd: str
