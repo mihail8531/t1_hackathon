5927e8049abfc29631871bcaa67454ecb55de81d
valery <valery.gusso@yandex.ru>
2024-11-27 21:41:01 +0300

    add admin_app

---
+++ admin_app/src/components/layouts/base.vue
@@ -0,0 +1,17 @@
+<template>
+  <main :class="$style.container">
+    <slot name="default" />
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" module>
+.container {
+  width: 100%;
+  height: 100dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ admin_app/src/pages/root.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="$style.container">
+    <SelectButton v-model="value" :options="options" optionLabel="label" :unselectable="false" :multiple="false" />
+
+    <Divider />
+
+    <component :is="value.component" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, shallowRef } from 'vue';
+
+import SelectButton from 'primevue/selectbutton';
+import Divider from 'primevue/divider';
+
+import FilesUploader from '@/components/filesUploader.vue';
+import ConnectToSQL from '@/components/connectToSQL.vue';
+
+const options = ref([
+  { label: 'Загрузить файлы', component: shallowRef(FilesUploader) },
+  { label: 'Подключиться к SQL базе данных', component: shallowRef(ConnectToSQL) }
+]);
+const value = ref(options.value[0]);
+</script>
+
+<style lans="scss" module>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 36px;
+  border-radius: 12px;
+  box-shadow:
+    rgba(0, 0, 0, 0.19) 0px 10px 20px,
+    rgba(0, 0, 0, 0.23) 0px 6px 6px;
+}
+</style>
