f56137fcae478cec1a331fd2ee571e379591ae8e
Егор Марков <e.markov@gt-sot.ru>
2024-11-28 11:45:23 +0300

    FEATURE: add documnets ids in response

--- admin_backend/app/file_services/api.py
+++ admin_backend/app/file_services/api.py
@@ -66,6 +66,74 @@ async def upload_new_file(
                 permission=permission,
                 chunk_method=chunk_method,
             )
+            docs_ids = []
+            docs = new_ds.upload_documents(_data)
+            for doc in docs:
+                docs_ids.append(doc.id)
+            rsp = {"dataset_id": new_ds.id, "docs": docs_ids}
+            return JSONResponse(rsp)
+        except Exception as err:
+            app_logger.error(f"{__name__}: {err}")
+            return JSONResponse(
+                {"error": 500}, status_code=http.HTTPStatus.INTERNAL_SERVER_ERROR
+            )
+
+    try:
+        ds = flow.list_datasets(id=dataset_id)
+        if len(ds) == 0:
+            app_logger.error(f"{__name__}: dataset {dataset_id} not found")
+            return JSONResponse({"error": 500}, status_code=http.HTTPStatus.NOT_FOUND)
+        docs_ids = []
+        docs = ds[0].upload_documents(_data)
+        for doc in docs:
+            docs_ids.append(doc.id)
+        rsp = {"dataset_id": ds[0].id, "docs": docs_ids}
+        return JSONResponse(rsp)
+    except Exception as err:
+        app_logger.error(f"{__name__}: {err}")
+        return JSONResponse(
+            {"error": 500}, status_code=http.HTTPStatus.INTERNAL_SERVER_ERROR
+        )
+
+
+@files_uploader.post("/new/files")
+async def upload_new_files(
+    rag_settings: Annotated[Settings, Depends(get_settings)],
+    files: list[UploadFile],
+    filename: str,
+    description: str,
+    language: str,
+    permission: str,
+    chunk_method: str,
+    avatar: Optional[str] = "",
+    dataset_id: Optional[str] = None,
+) -> JSONResponse:
+    """upload limit = 20"""
+    flow = RAGFlow(
+        api_key=rag_settings.RAGFLOW_API_KEY, base_url=rag_settings.ragflow_base_url
+    )
+    _data = []
+    for file in files:
+        extension = get_extension(file.filename or "")
+        if extension not in SUPPORTED_EXTS:
+            raise HTTPException(status_code=415, detail="wrong extension")
+        converter = get_converter(file)
+        converted_file = converter.convert_file(
+            File(content=file.file, name=file.filename or "")
+        )
+        _data.append(
+            {"displayed_name": converted_file.name, "blob": converted_file.content},
+        )
+    if dataset_id is None:
+        try:
+            new_ds = flow.create_dataset(
+                name=filename,
+                avatar=avatar,
+                description=description,
+                language=language,
+                permission=permission,
+                chunk_method=chunk_method,
+            )
             new_ds.upload_documents(_data)
             rsp = {"dataset_id": new_ds.id}
             return JSONResponse(rsp)
@@ -82,9 +150,34 @@ async def upload_new_file(
             return JSONResponse({"error": 500}, status_code=http.HTTPStatus.NOT_FOUND)
         ds[0].upload_documents(_data)
         rsp = {"dataset_id": ds[0].id}
-        return JSONResponse(rsp)
+        JSONResponse(rsp)
     except Exception as err:
         app_logger.error(f"{__name__}: {err}")
         return JSONResponse(
             {"error": 500}, status_code=http.HTTPStatus.INTERNAL_SERVER_ERROR
         )
+
+
+# @files_uploader.get("/files")
+# async def get_files(
+#     rag_settings: Annotated[Settings, Depends(get_settings)], dataset_id: str
+# ) -> JSONResponse:
+#     flow = RAGFlow(
+#         api_key=rag_settings.RAGFLOW_API_KEY, base_url=rag_settings.ragflow_base_url
+#     )
+#
+#     if dataset_id == "":
+#         app_logger.error(f"{__name__}: no dataset id")
+#         return JSONResponse({"error": 400}, status_code=http.HTTPStatus.BAD_REQUEST)
+#
+#     try:
+#         ds = flow.list_datasets(id=dataset_id)
+#         if len(ds) == 0:
+#             return JSONResponse([])
+#
+#         JSONResponse(rsp)
+#     except Exception as err:
+#         app_logger.error(f"{__name__}: {err}")
+#         return JSONResponse(
+#             {"error": 500}, status_code=http.HTTPStatus.INTERNAL_SERVER_ERROR
+#         )
